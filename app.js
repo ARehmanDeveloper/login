3ba6a7e5f7ca9ca3ea69a8f3a71129baa382fde4
ARehmanDeveloper <ar8047591@gmail.com>
2025-07-31 03:30:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const SUPABASE_URL = 'https://jzgqfkbmudjblitvevxc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Z3Fma2JtdWRqYmxpdHZldnhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzMzMDAsImV4cCI6MjA2OTA0OTMwMH0.bUva-RMXg_rX_ecx54EJCm99G7ASlzVy5nxS92t16Ec';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginBtn = document.getElementById('loginBtn');
+  if (loginBtn) {
+    loginBtn.addEventListener('click', async () => {
+      const email = document.getElementById('loginEmail').value;
+      const password = document.getElementById('loginPassword').value;
+      const msg = document.getElementById('loginMessage');
+
+      const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+      if (error) {
+        msg.textContent = error.message;
+        msg.className = 'msg error';
+      } else {
+        msg.textContent = 'Login successful!';
+        msg.className = 'msg success';
+        setTimeout(() => window.location.href = 'dashboard.html', 1500);
+      }
+    });
+  }
+
+  
+  const signupBtn = document.getElementById('signupBtn');
+  if (signupBtn) {
+    signupBtn.addEventListener('click', async () => {
+      const email = document.getElementById('signupEmail').value;
+      const password = document.getElementById('signupPassword').value;
+      const msg = document.getElementById('signupMessage');
+
+      const { error } = await supabase.auth.signUp({ email, password });
+
+      if (error) {
+        msg.textContent = error.message;
+        msg.className = 'msg error';
+      } else {
+        msg.textContent = 'Signup successful!';
+        msg.className = 'msg success';
+        setTimeout(() => window.location.href = 'index.html', 1500);
+      }
+    });
+  }
+});
+
+const googleLogin = document.getElementById('googleLogin');
+if (googleLogin) {
+  googleLogin.addEventListener('click', async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'http://localhost:5500/dashboard.html'
+
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const facebookLogin = document.getElementById('facebookLogin');
+
+  if (facebookLogin) {
+    facebookLogin.addEventListener('click', async () => {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'facebook',
+        options: {
+          redirectTo: 'http://localhost:5500/dashboard.html'
+        }
+      });
+
+      if (error) {
+        console.error('Facebook Login Error:', error.message);
+      }
+    });
+  }
+});
